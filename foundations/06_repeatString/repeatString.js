9e3a892b283bc535179ffc039ee246398ea8c600
mro <168573711+Marula3021@users.noreply.github.com>
2026-02-16 16:09:36 +0100

    format javascript document

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,13 +1,13 @@
 const repeatString = function (str, num) {
-  let result = "";
+    let result = "";
 
-  for (let i = 0; i < num; i++) {
-    if (num < 0){
-        result = "ERROR"
-    }else
-    result += str;
-  }
-  return result;
+    for (let i = 0; i < num; i++) {
+        if (num < 0) {
+            result = "ERROR"
+        } else
+            result += str;
+    }
+    return result;
 };
 
 // Do not edit below this line
