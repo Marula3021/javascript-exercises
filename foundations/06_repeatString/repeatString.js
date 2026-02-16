d0d7743c741135b33e469f078f2dc43fc89b0694
mro <168573711+Marula3021@users.noreply.github.com>
2026-02-16 16:05:28 +0100

    add ERROR message , if repeatString number is < 0

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -2,6 +2,9 @@ const repeatString = function (str, num) {
   let result = "";
 
   for (let i = 0; i < num; i++) {
+    if (num < 0){
+        result = "ERROR"
+    }else
     result += str;
   }
   return result;
