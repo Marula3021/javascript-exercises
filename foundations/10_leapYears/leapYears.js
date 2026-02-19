22ef83ab82765846becebbaf96776fac49577aeb
mro <168573711+Marula3021@users.noreply.github.com>
2026-02-19 17:06:58 +0100

    My solution
    - Add alternative version

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,17 @@
 const leapYears = function (year) {
-  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+  let result = false;
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    result = true;
+  } else {
+    result = false;
+  }
+  return result;
 };
+
+
+//      Alternative solution without if 
+// const leapYears = function (year) {
+// return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 
 // Do not edit below this line
 module.exports = leapYears;
