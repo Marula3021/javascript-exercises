cb753055e9a29c068c76632053b288415c1e8131
mro <168573711+Marula3021@users.noreply.github.com>
2026-02-19 17:54:18 +0100

    My solution Use Math.round method to round the result to one decimal

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (f) {
+  const Fahrenheit = ((f - 32) * 5) / 9;
+  return Math.round(Fahrenheit * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (c) {
+  const celsius = (c * 9) / 5 + 32
+  return Math.round(celsius * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
