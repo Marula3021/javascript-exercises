1179b1fdbcd9f4d75dd77e63f5550f671aa048a9
mro <168573711+Marula3021@users.noreply.github.com>
2026-02-25 11:44:32 +0100

    Update Solution Use ternary operator

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,10 +1,5 @@
 const leapYears = function (year) {
-  let result = false;
-  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
-    result = true;
-  } else {
-    result = false;
-  }
+  result = ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) ? true : false ;
   return result;
 };
 
